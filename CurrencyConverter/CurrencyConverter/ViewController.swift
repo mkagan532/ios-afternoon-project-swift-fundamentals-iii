0721a88edbf872215c45fd35c4684e44c05a2c7d
Michael Kagan <mkagan532@gmail.com>
2020-01-24 00:09:51 -0500

    Modified steps 1, 3, 4, and 5

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum Currency {
+    case cad
+    case mxn
+}
+
 class ViewController: UIViewController {
     
     // MARK: - Outlets/Properties
@@ -23,17 +28,51 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        guard let usdTextFieldInput = fromCurrencyTextField.text
+        let usdAmount = Double(usdTextFieldInput); else { return }
         
+        var convertedAmount: Double = 0
+        if mxnButton.isSelected == true {
+            convertedAmount = convert(dollars: usdAmount, to: .cad)
+        }
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
-        
+        if sender == mxnButton{
+            mxnButton.isSelected = true
+            cadButton.isSelected = false
+            toCurrencyLabel.text = "Currency (MXN)"
+        } else if sender == cadButton{
+            mxnButton.isSelected = false
+            cadButton.isSelected = true
+            toCurrencyLabel.text = "Currency (CAD)"
+        }
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
-        
+        if sender == mxnButton{
+            mxnButton.isSelected = true
+            cadButton.isSelected = false
+            toCurrencyLabel.text = "Currency (MXN)"
+        } else if sender == cadButton{
+            mxnButton.isSelected = false
+            cadButton.isSelected = true
+            toCurrencyLabel.text = "Currency (CAD)"
+        }
     }
     
     // MARK: - Helper Methods
+    func convert(dollars: Double, to unit: Currency) -> Double {
+        let money: Double = dollars
+        var conversion: Double = 0
+        
+        switch unit {
+        case .cad:
+            conversion = money * 1.35
+        case .mxn:
+            conversion = money * 19.78
+        }
+        return conversion
+    }
 }
 
